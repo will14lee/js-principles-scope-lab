57f632c55d166bdb74190d52cde2f05726fdb5b0
will14lee <will14lee@gmail.com>
2021-06-10 11:08:23 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName="bob";
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase()
+};
+
+
+console.log(upperCaseCustomerName)
+
+function setBestCustomer(){
+    return bestCustomer= "not bob";
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer= "maybe bob";
+}
+
+const leastFavoriteCustomer=("Angela");
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer=("Gori");
+}
+
